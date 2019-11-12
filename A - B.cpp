28a108ce07feec73be66c44d89b6a7653d3803d6
Vladislav Lukashevich <43114166+vlukashevich@users.noreply.github.com>
2019-11-12 16:35:04 +0300

    Update A - B.cpp

--- A - B.cpp
+++ A - B.cpp
@@ -2,24 +2,31 @@
 #include <cstdlib>
 using namespace std;
 
-int a[1001], b[1001], c[1001];
+int n, m, a[1001], b[1001], c[1001];
 string s1, s2;
 int main()
 {
     cin >> s1 >> s2;
 
-    if (s2.size() > s1.size()) {
+    n = s1.size();
+    m = s2.size();
+    
+    if (m > n) {
         string t = s1;
         s1 = s2;
         s2 = t;
+        n = s1.size();
+        m = s2.size();
         cout << "-";
-    } else if (s2.size() == s1.size()) {
-        for (int i = 0; i < s1.size(); i++) {
+    } else if (n == m) {
+        for (int i = 0; i < n; i++) {
             if (s2[i] != s1[i]) {
                 if (s2[i] > s1[i]) {
                     string t = s1;
                     s1 = s2;
                     s2 = t;
+                    n = s1.size();
+                    m = s2.size();
                     cout << "-";
                 }
                 break;
@@ -27,16 +34,16 @@ int main()
         }
     }
 
-    for (int i = 0; i < s1.size(); i++) {
+    for (int i = 0; i < n; i++) {
         a[i] = 0;
         b[i] = 0;
         c[i] = 0;
     }
 
-    for (int i = 0; i < s1.size(); i++) a[i] = atoi(s1.substr(s1.size() - i - 1, 1).c_str());
-    for (int j = 0; j < s2.size(); j++) b[j] = atoi(s2.substr(s2.size() - j - 1, 1).c_str());
+    for (int i = 0; i < n; i++) a[i] = atoi(s1.substr(n - i - 1, 1).c_str());
+    for (int j = 0; j < m; j++) b[j] = atoi(s2.substr(m - j - 1, 1).c_str());
 
-    for (int i = 0; i < s1.size(); i++) {
+    for (int i = 0; i < n; i++) {
         c[i] += a[i] - b[i];
 
         if (c[i] < 0) {
@@ -45,10 +52,10 @@ int main()
         }
     }
 
-    int k = s1.size() - 1;
-    while (c[k] == 0 && k > 0) k--;
+    n--;
+    while (c[n] == 0 && n > 0) n--;
 
-    for (int j = k; k > -1; k--) cout << c[k];
+    for (; n > -1; n--) cout << c[n];
 
     return 0;
 }
